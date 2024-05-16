7cd159729ad85e68f4397fbb4dd962070b902ae5
minyeongnam <minyn0106@gmail.com>
2024-05-16 17:53:09 +0900

    feature: SyntaxHighlighter 추가

--- src/components/MarkdownViewer.tsx
+++ src/components/MarkdownViewer.tsx
@@ -1,7 +1,8 @@
 import Markdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
-import { dark } from "react-syntax-highlighter/dist/esm/styles/prism";
+import { darcula } from "react-syntax-highlighter/dist/esm/styles/prism";
+import Image from "next/image";
 
 interface Props {
   content: string;
@@ -12,26 +13,36 @@ export default function MarkdownViewer({ content }: Props) {
     <Markdown
       className="markdown-viewer"
       remarkPlugins={[remarkGfm]}
-      // components={{
-      //   code(props) {
-      //     const { children, className, node, ...rest } = props;
-      //     const match = /language-(\w+)/.exec(className || "");
-      //     return match ? (
-      //       <SyntaxHighlighter
-      //         {...rest}
-      //         PreTag="div"
-      //         language={match[1]}
-      //         style={dark}
-      //       >
-      //         {String(children).replace(/\n$/, "")}
-      //       </SyntaxHighlighter>
-      //     ) : (
-      //       <code {...rest} className={className}>
-      //         {children}
-      //       </code>
-      //     );
-      //   },
-      // }}
+      components={{
+        code(props) {
+          const { children, className, node, ...rest } = props;
+          const match = /language-(\w+)/.exec(className || "");
+          return match ? (
+            <SyntaxHighlighter
+              {...rest}
+              PreTag="div"
+              language={match[1]}
+              style={darcula}
+              ref={null}
+            >
+              {String(children).replace(/\n$/, "")}
+            </SyntaxHighlighter>
+          ) : (
+            <code {...rest} className={className}>
+              {children}
+            </code>
+          );
+        },
+        img: (image) => (
+          <Image
+            src={image.src || ""}
+            alt={image.alt || ""}
+            className="markdown-viewer_image"
+            width={500}
+            height={350}
+          />
+        ),
+      }}
     >
       {content}
     </Markdown>
